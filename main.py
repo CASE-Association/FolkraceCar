3aff1f780cf3f208d7e69fe138a9abdb7f9290ad
Stefan Larsson <stefanlarsson95@gmail.com>
2020-02-23 18:01:41 +0100

    imp concep ground_plane estimator in path_planner

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ TODO
 '''
 import pyrealsense2 as rs
 import numpy as np
+from scipy.optimize import leastsq as LS
+from numpy.linalg import svd
 import cv2
 import math
 import time
@@ -26,7 +28,7 @@ import threading as mt
 frame_width = 640
 frame_height = 480
 fps = 30
-_camera_car_offset = [0.01, 0, -2.05]  # The offset from camera to car pivot point. [w h l]
+_camera_car_offset = [0.01, 0, -2.05]  # The offset from camera to car pivot point. [w h l] # todo conv to understandable values?
 _car_size = [0.175, 0.1, 0.3, 0.05]  # [w h l r]
 blind = 0  # For debugging w/o camera
 
@@ -36,10 +38,12 @@ class Folkracer:
         """
         Main Car object.
         Defines the model of car and camera position relation.
+        Holds current value of speed and steering angle.
+        These can be passed from e.g. a Path planner to control the Car.
         :param car_size: dimension of car in pointcloud dimension
         :param camera_car_offset: offsent from car origin to camera origin
         """
-        #  Def car size [width, height, length, wheel radius]
+        #  Define car size [width, height, length, wheel radius]
         if not car_size:
             self.size = [0.2, 0.1, 0.3, 0.05]
         else:
@@ -79,13 +83,30 @@ class Folkracer:
 
 
 class PathPlanner:
-    def __init__(self, Car):
+    def __init__(self, pipeline):
         """
         Gradient decent solving path planner ## todo Evaluate against other methods of path planning
         :param Car: Object to give driving vector
         """
-        self.Car = Car
+        self.pipe = pipeline
         self.running = False
+        self.decimate_val  = 0.1 # todo get good value
+        # Processing blocks
+        self.pc = rs.pointcloud()
+        self.decimate = rs.decimation_filter()
+        #self.decimate.set_option(rs.option.filter_magnitude, 2 ** self.decimate_val)
+        # self.colorizer = rs.colorizer()
+        self.w = 0
+        self.h = 0
+        self.ground_plane = self.get_ground_plane()
+        try:
+            # Get stream profile and camera intrinsics
+            profile = self.pipe.get_active_profile()
+            depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+            depth_intrinsics = depth_profile.get_intrinsics()
+            self.w, self.h = depth_intrinsics.width, depth_intrinsics.height
+        except RuntimeError as err:
+            print(err)
 
     def start(self):
         self.running = True
@@ -99,10 +120,82 @@ class PathPlanner:
     def end(self):
         self.running = False
 
+    def get_ground_plane(self):
+        """
+        Estimate ground plain using Least square
+        todo evaluate usage of RANSAC
+        :return: groudn plain origin and rotation
+        """
+
+        pos, normal = self.planeFit()
+
+        rot = np.eye(3)  # todo calculate rotation matrix
+
+        return pos, rot, normal
+
+    def planeFit(self):
+        """
+        Original code from: https://stackoverflow.com/a/18968498
+
+        p, n = planeFit(points)
+
+        Given an array, points, of shape (d,...)
+        representing points in d-dimensional space,
+        fit an d-dimensional plane to the points.
+
+        :return: a point, p, on the plane (the point-cloud centroid),
+        and the normal, n.
+        """
+        points = self.process_cloud()
+
+        points = np.reshape(points, (np.shape(points)[0], -1))  # Collapse trialing dimensions
+        assert points.shape[0] <= points.shape[1], "There are only {} points in {} dimensions.".format(points.shape[1],
+                                                                                                       points.shape[0])
+        ctr = points.mean(axis=1)
+        x = points - ctr[:, np.newaxis]
+        M = np.dot(x, x.T)  # Could also use np.cov(x) here.
+        return ctr, svd(M)[0][:, -1]
+
+    def process_cloud(self):
+        try:
+            # Wait for a coherent pair of frames: depth and color
+            frames = self.pipe.wait_for_frames()
+
+            depth_frame = frames.get_depth_frame()
+
+            depth_frame = self.decimate.process(depth_frame)
+
+            # Grab new intrinsics (may be changed by decimation)
+            depth_intrinsics = rs.video_stream_profile(
+                depth_frame.profile).get_intrinsics()
+            w, h = depth_intrinsics.width, depth_intrinsics.height
+
+            depth_image = np.asanyarray(depth_frame.get_data())
+
+            #depth_colormap = np.asanyarray(
+            #    colorizer.colorize(depth_frame).get_data())
+
+            #if state.color:
+            #    mapped_frame, color_source = color_frame, color_image
+            #else:
+            #    mapped_frame, color_source = depth_frame, depth_colormap
+
+            points = self.pc.calculate(depth_frame)
+            #self.pc.map_to(mapped_frame)
+
+            # Pointcloud data to arrays
+            v, t = points.get_vertices(), points.get_texture_coordinates()
+            verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
+            #texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+        except:
+            verts = np.zeros((3, 3))
+
+        return np.transpose(verts)
+
     def path_plan(self):
         while self.running:
-            print(self.Car.steer)
-            time.sleep(1)
+            self.ground_plane = self.get_ground_plane()
+            time.sleep(0.1)
 
         print('Path planner ended')
 
@@ -169,7 +262,7 @@ def rs_init(width=640, height=480, fps=30):
     return pipeline
 
 
-def viewer(pipeline, Car, camera=True):
+def viewer(pipeline, Car,path_planner, camera=True, draw=None):
     """
     OpenCV and Numpy Point cloud Software Renderer
 
@@ -194,6 +287,8 @@ def viewer(pipeline, Car, camera=True):
         [q\ESC] Quit
     """
 
+    if draw is None:  # Draw all
+        draw = ['car', 'grid', 'axes', 'frustum']
     state = AppState(Car.camera_offset)
 
     try:
@@ -496,7 +591,8 @@ def viewer(pipeline, Car, camera=True):
 
         out.fill(0)
 
-        axes(out, view(np.zeros((1, 3))), state.rotation, size=0.1, thickness=1)
+        if 'axes'in draw:
+            axes(out, view(np.zeros((1, 3))), state.rotation, size=0.1, thickness=1)
 
         if pc:
             if not state.scale or out.shape[:2] == (h, w):
@@ -508,12 +604,19 @@ def viewer(pipeline, Car, camera=True):
                     tmp, out.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
                 np.putmask(out, tmp > 0, tmp)
         # todo get grid to show est "driving plane"
-        grid(out, pos=[state.offset[0],
+        if 'grid' in draw:
+            if path_planner:
+                ground_pos = path_planner.ground_plane[0]  # Get new est center of plane
+            else:
+                ground_pos = [state.offset[0],
                        state.offset[1] + Car.size[1] / 2 + Car.size[3],
-                       state.offset[2] + 2], size=1, n=15)
+                       state.offset[2] + 2]
+            grid(out, pos=ground_pos, size=1, n=15)
 
         # fixme get np.dot to work and hardcoded +2 offset
-        car_model(out, pos=[state.offset[0], state.offset[1], state.offset[2] + 2], car_size=Car.size)
+        if 'car' in draw:
+            car_model(out, pos=[state.offset[0], state.offset[1], state.offset[2] + 2], car_size=Car.size)
+
         if pc: frustum(out, depth_intrinsics)
 
         if any(state.mouse_btns):
@@ -578,17 +681,19 @@ def main():
 
     # Creat Car and Path planner
     Car = Folkracer(car_size=_car_size, camera_car_offset=_camera_car_offset)
-    pp = PathPlanner(Car)
+    pp = PathPlanner(pipeline=pl)
 
     # Start threads
     #Car.start()
-    #pp.start()
+    pp.start()
 
+    #print(pp.planeFit())
 
-    viewer(pl, Car, not blind)
+    viewer(pl, Car, pp, not blind, ['pointcloud', 'frustum', 'axes', 'grid'])
 
-    # Finish, close Car handler
+    # Finish, close Car handler and Path planner
     Car.end()
+    pp.end()
 
 
 if __name__ == '__main__':
